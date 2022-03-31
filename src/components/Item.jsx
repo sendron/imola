a823d3c8ae3d0138fd19178bd40dd600193c80a3
Jon Inge Nordnes <joninge@allegro.no>
2022-03-31 14:18:45 +0200

    Fixed background image

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,10 +7,13 @@ const Item = (props) => {
 
     let date = new Date(parseInt(lap.laptime));
 
+
+    // console.log(lap.steamProfile.avatar.large )
+
     return (
         <div className={c}>
             <div className="pos">{pos}</div>
-            <div className="pic" style={{backgroundImage: `url(${lap.steamProfile.avatar.large})`}}></div>
+            <div className="pic" style={`background-image: url(${lap.steamProfile.avatar.large})`}></div>
             <div className="name">{lap.steamProfile.nickname}</div>
             <div className="score vehicle">{lap.vehiclename}</div>
             <div className="score">{`${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()}`}</div>
